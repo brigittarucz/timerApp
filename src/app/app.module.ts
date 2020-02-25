b7481173d96ac55f447e59d4b2d8ed348fc436f8
Brigitta <brigitta.rucz@yahoo.com>
2020-02-25 15:57:11 +0100

    profile quickview migration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { MenuSidebarComponent } from './menu-sidebar/menu-sidebar.component';
+import { ProfileQuickviewComponent } from './profile-quickview/profile-quickview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    MenuSidebarComponent
+    MenuSidebarComponent,
+    ProfileQuickviewComponent
   ],
   imports: [
     BrowserModule
