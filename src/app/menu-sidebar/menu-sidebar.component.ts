4f47d576cd4ee6587f99d69808c78a6d2bdc8fe0
Brigitta <brigitta.rucz@yahoo.com>
2020-02-25 11:13:44 +0100

    navbar migration

--- src/app/menu-sidebar/menu-sidebar.component.ts
+++ src/app/menu-sidebar/menu-sidebar.component.ts
@@ -6,19 +6,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: [ './menu-sidebar.component.css' ]
 })
 export class MenuSidebarComponent implements OnInit {
-	toggleNav = false;
-	displayNav = 'none';
-
 	constructor() {}
-
-	toggleNavbar() {
-		this.toggleNav = !this.toggleNav;
-		if (this.toggleNav) {
-			this.displayNav = 'block';
-		} else {
-			this.displayNav = 'none';
-		}
-	}
 
 	ngOnInit(): void {}
 }
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navigation-bar',
-  templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.css']
+	selector: 'app-navigation-bar',
+	templateUrl: './navigation-bar.component.html',
+	styleUrls: [ './navigation-bar.component.css' ]
 })
 export class NavigationBarComponent implements OnInit {
+	toggleNav = false;
+	displayNav = 'none';
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	toggleNavbar() {
+		this.toggleNav = !this.toggleNav;
+		if (this.toggleNav) {
+			this.displayNav = 'block';
+		} else {
+			this.displayNav = 'none';
+		}
+	}
 
+	ngOnInit(): void {}
 }
