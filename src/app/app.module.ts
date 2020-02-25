382567e0c0f27f49e8b5149d7a9ec85407f97243
Brigitta <brigitta.rucz@yahoo.com>
2020-02-25 11:07:48 +0100

    assets and generic sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { MenuSidebarComponent } from './menu-sidebar/menu-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    MenuSidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/menu-sidebar/menu-sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-menu-sidebar',
+	templateUrl: './menu-sidebar.component.html',
+	styleUrls: [ './menu-sidebar.component.css' ]
+})
+export class MenuSidebarComponent implements OnInit {
+	blue = 'blue';
+	constructor() {}
+
+	ngOnInit(): void {}
+}
