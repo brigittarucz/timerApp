0b79e031b941de68dd99ee0d6f3f9e20023514f4
Brigitta <brigitta.rucz@yahoo.com>
2020-03-02 17:05:32 +0100

    sidebar desktop click

--- src/app/menu-sidebar-desktop/menu-sidebar-desktop.component.ts
+++ src/app/menu-sidebar-desktop/menu-sidebar-desktop.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-menu-sidebar-desktop',
-  templateUrl: './menu-sidebar-desktop.component.html',
-  styleUrls: ['./menu-sidebar-desktop.component.css']
+	selector: 'app-menu-sidebar-desktop',
+	templateUrl: './menu-sidebar-desktop.component.html',
+	styleUrls: [ './menu-sidebar-desktop.component.css' ]
 })
 export class MenuSidebarDesktopComponent implements OnInit {
+	itIsTrue = true;
+	selectedButton;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	classesArr = [
+		'icon_tracking',
+		'icon_user-history',
+		'icon_activity',
+		'icon_project-history',
+		'icon_form-setting',
+		'icon_users',
+		'icon_clients-projects',
+		'icon_user-access'
+	];
 
+	classesImgArr = [
+		'tracking-icon',
+		'userhistory-icon',
+		'activity-icon',
+		'projectshistory-icon',
+		'taskformsettings-icon',
+		'users-icon',
+		'clients&projects-icon',
+		'useraccess-icon'
+	];
+
+	ngOnInit(): void {}
+
+	onSelect(clicked) {
+		this.selectedButton = clicked.target.classList[1];
+	}
 }
