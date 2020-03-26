0ec7badd61b6d3dff09a0b7155b090769f85b2d9
karolinakv <karolinavalentova2@gmail.com>
2020-03-26 02:18:40 +0100

    users page - added data table draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import {Providers} from "./app.providers";
 import {Pipes} from "./app.pipes";
 import { FormSettingTableDataComponent } from './components/ui-artifacts/form-setting/form-setting-table-data/form-setting-table-data.component';
 import { FormSettingNavigationComponent } from './components/ui-artifacts/form-setting/form-setting-navigation/form-setting-navigation.component';
+import { UsersFilterFormComponent } from './components/views/users/users-filter-form/users-filter-form.component';
+import { UsersDataTableComponent } from './components/views/users/users-data-table/users-data-table.component';
 
 
 @NgModule({
@@ -15,6 +17,8 @@ import { FormSettingNavigationComponent } from './components/ui-artifacts/form-s
     ...Pipes,
     FormSettingTableDataComponent,
     FormSettingNavigationComponent,
+    UsersFilterFormComponent,
+    UsersDataTableComponent,
   ],
   imports: [ ...Imports ],
   providers: [ ...Providers],
